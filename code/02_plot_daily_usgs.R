46a17bc1c7003dfbdf20bbd7f39b3791a8fbecd3
Ryan Peek <rapeek@ucdavis.edu>
2021-07-28 22:48:13 -0700

    update plots w gages

--- code/02_plot_daily_usgs.R
+++ code/02_plot_daily_usgs.R
@@ -8,7 +8,8 @@ library(tidyverse)
 library(mapview)
 library(purrr)
 library(lubridate)
-library(tidylog)
+library(glue)
+#library(tidylog)
 library(ggdark)
 
 # GET SITES ---------------------------------------------------------------
@@ -24,35 +25,32 @@ table(all_gages$refgage)
 # unique? (n=748)
 length(unique(all_gages$gageid))
 
-ref_only <- all_gages %>% filter(refgage=="Ref")
+gages <- all_gages %>% filter(refgage=="Non-Ref")
 
 # Get Data ----------------------------------------------------------------
 
 load("data/usgs_Q_daily_all_gages.rda")
 
 # filter to just ref
-usgs_flows_ref <- usgs_flows %>% 
-  filter(site_no %in% ref_only$gageid) %>% 
+usgs_flows_meta <- usgs_flows %>% 
+  filter(site_no %in% gages$gageid) %>% 
   select(-agency_cd) %>% 
   left_join(., usgs_meta, by=c("site_no"="site_id"))
 
+length(unique(usgs_flows_meta$site_no))
 
-# Get Date Ranges ---------------------------------------------------------
-
-usgs_flows_ref_span <- usgs_flows_ref %>% 
-  select(site_no, station_nm:yr_total) %>% 
-  distinct(.keep_all=TRUE)
+# rm usgs_flows
+rm(usgs_flows)
 
 # VISUALIZE DATE RANGES FOR SITES -----------------------------------------
 
 # visualize the stations to see date ranges
 ggplot() + 
-  geom_linerange(data=usgs_flows_ref_span, 
-                 aes(x=forcats::fct_reorder(site_no, huc8), ymin=yr_begin, ymax=yr_end, color=as.factor(huc8)), size=1.1, show.legend = F) + 
+  geom_linerange(data=usgs_meta, 
+                 aes(x=forcats::fct_reorder(site_id, huc8), ymin=yr_begin, ymax=yr_end, color=as.factor(huc8)), size=1.1, show.legend = F) + 
   coord_flip() + 
   labs(x="", y="") + 
-  scale_color_viridis_d(option = "A") +
-  #theme_bw(base_family = "Roboto Condensed", base_size = 8) +
+  scale_color_viridis_d(option = "D") +
   ggdark::dark_theme_bw(base_family = "Roboto Condensed", base_size = 8)
 
 # ggsave(filename = "figures/usgs_gage_year_ranges.jpg",
@@ -61,8 +59,53 @@ ggplot() +
 #ggsave(filename = "figures/usgs_gages_year_ranges.pdf", 
 #       width=11, height = 8.5, units = "in", device = cairo_pdf)
 
-
 # Visualize Flow Data -----------------------------------------------------
 
+# expects x and y to be date and temp, but specify col name in quotes
+plot_gage_facet <- function(data, x, y, facetid, plotly=FALSE){
+  if(plotly == TRUE){
+    p1 <- ggplot() +
+      geom_line(data=data,
+                aes(x=.data[[x]], y=.data[[y]],
+                    group=.data[[facetid]], color=.data[[facetid]]),
+                show.legend = F) +
+      theme_classic(base_family = "Roboto Condensed", base_size = 9) +
+      scale_color_viridis_d() + labs(y="Flow (cfs)", x="") +
+      theme(axis.text.x = element_text(angle=90, hjust = 1)) +
+      facet_wrap(.~.data[[facetid]], scales= "free_x")
+    cat("Plotly it is!")
+    return(plotly::ggplotly(p1))
+  }
+  p2 <- ggplot() +
+    geom_line(data=data,
+              aes(x=.data[[x]], y=.data[[y]],
+                  group=.data[[facetid]], color=.data[[facetid]]),
+              show.legend = F) +
+    theme_classic(base_family = "Roboto Condensed", base_size = 9) +
+    scale_color_viridis_d() + labs(y="Flow (cfs)", x="") +
+    theme(axis.text.x = element_text(angle=90, hjust = 1)) +
+    facet_wrap(.~.data[[facetid]], scales= "free")
+  cat("printing static ggplots...")
+  return(print(p2))
+}
 
-usgs_flows_ref %>% 
+# get list of gages and facet plot
+usgs_flows_meta %>% filter(site_no %in% gages$gageid[30:41]) %>% 
+  plot_gage_facet(., x="Date", y="Flow", 
+                  facetid = "site_no", plotly = F)
+
+# single gage
+g1 <- usgs_flows_meta %>% filter(site_no %in% gages$gageid[20])
+
+ggplot(data=g1) +
+  geom_line(aes(x=Date, 
+                #y=Flow,
+                y=log(Flow), 
+                group=site_no, color=site_no), show.legend = F) +
+  labs(subtitle = glue("USGS {g1$site_no}: {g1$station_nm}")) +
+  theme_classic(base_family = "Roboto Condensed", base_size = 9) +
+  scale_color_viridis_d() + labs(y="Flow (cfs)", x="") +
+  theme(axis.text.x = element_text(angle=90, hjust = 1))
+
+# save
+#ggsave(filename = "figs/hydrographs_highly_alt_gageids_1-20.png", width = 11, height = 8.5, dpi=300)
